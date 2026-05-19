05c5871f10eb70f7505f6569e5397ae856305e17
nafilaraya02-lab <nafilaraya02@gmail.com>
2026-05-19 15:03:04 +0700

    stud

---
+++ lib/models/civ_pengaturan_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class CivPengaturanModel {
+  final String title;
+  final IconData icon;
+  final String subtitle;
+
+  CivPengaturanModel({
+    required this.title,
+    required this.icon,
+    required this.subtitle,
+  });
+}
---
+++ lib/services/civ_service_pengaturan.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:studmethods/models/civ_pengaturan_model.dart';
+
+List<CivPengaturanModel> siv = [
+  CivPengaturanModel(
+    title: 'Tema',
+    icon: Icons.text_fields,
+    subtitle: 'ATUR TEMA',
+  ),
+  CivPengaturanModel(
+    title: 'Colors',
+    icon: Icons.color_lens_outlined,
+    subtitle: 'MERUBAH WARNA',
+  ),
+  CivPengaturanModel(
+    title: 'Pasword',
+    icon: Icons.color_lens_outlined,
+    subtitle: 'RUBAH PASSWORD ',
+  ),
+  CivPengaturanModel(
+    title: 'Bantuan',
+    icon: Icons.help,
+    subtitle: 'MINTA BANTUAN KEPADA KAMI',
+  )
+];
